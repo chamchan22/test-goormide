bf3af3b3df8b0d635d8c4787fce71cff06047600
한 <hantalk98@gmail.com>
2021-09-04 10:22:28 +0000

    second push

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,13 +1,17 @@
 package project;
+import java.util.Scanner;
+import java.lang.Math;
 
 public class Main {
-
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.println("Hello goorm!");
+    
+    public static void main(String args[]) {
+        Scanner stdIn = new Scanner(System.in);
+        
+        System.out.print("실수값 : ");
+        double x = stdIn.nextDouble();
+        
+        System.out.println("절댓값 : " + Math.abs(x));
+        System.out.println("제곱근 : " + Math.sqrt(x));
+        System.out.println("원의 면적 : " + Math.PI * x * x);
     }
-
 }
